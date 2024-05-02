277fa1693fb2c8184a9c6e453aa57f16e43dc08b
mehrazrumman <mehrazrumman33@icloud.com>
2024-05-02 13:25:06 +0600

    initial commit

---
+++ frontend/src/products.js
@@ -0,0 +1,109 @@
+const products = [
+    {
+        '_id':'1',
+        'name':'Banana',
+        'image':'/images/kola.jpg',
+        'description':'Fruit',
+        'category':'Fruit',
+        'price':'10 TAKA',
+        'countInStock':1000,
+        'rating':4.5,
+        'numReviews':12,
+
+
+    },
+    {
+        '_id':'2',
+        'name':'Cabbage',
+        'image':'/images/cabbage.jpg',
+        'description':'Vegetables',
+        'category':'Vegetables',
+        'price':'20 TAKA',
+        'countInStock':1000,
+        'rating':5,
+        'numReviews':10,
+
+
+    },
+    {
+        '_id':'3',
+        'name':'Carrots',
+        'image':'/images/carrots.jpg',
+        'description':'Fruit',
+        'category':'Fruit',
+        'price':'15 TAKA',
+        'countInStock':20,
+        'rating':4.9,
+        'numReviews':12,
+
+
+    },
+    {
+        '_id':'4',
+        'name':'Eggplant',
+        'image':'/images/eggplant.jpg',
+        'description':'Fruit',
+        'category':'Fruit',
+        'price':'15 TAKA',
+        'countInStock':20,
+        'rating':4.9,
+        'numReviews':12,
+
+    },
+    {
+        '_id':'5',
+        'name':'Garlic',
+        'image':'/images/garlic.jpg',
+        'description':'Fruit',
+        'category':'Fruit',
+        'price':'15 TAKA',
+        'countInStock':20,
+        'rating':4.9,
+        'numReviews':12,
+
+
+    },
+    {
+        '_id':'6',
+        'name':'GreenBeans',
+        'image':'/images/greenbeans.jpg',
+        'description':'Fruit',
+        'category':'Fruit',
+        'price':'15 TAKA',
+        'countInStock':20,
+        'rating':4.9,
+        'numReviews':12,
+
+
+    },
+    {
+        '_id':'7',
+        'name':'Mashrooms',
+        'image':'/images/mashrooms.jpg',
+        'description':'Fruit',
+        'category':'Fruit',
+        'price':'15 TAKA',
+        'countInStock':20,
+        'rating':4.9,
+        'numReviews':12,
+
+    },
+    {
+        '_id':'8',
+        'name':'Tomatoes',
+        'image':'/images/tomatos.jpg',
+        'description':'Fruit',
+        'category':'Fruit',
+        'price':'15 TAKA',
+        'countInStock':20,
+        'rating':4.9,
+        'numReviews':12,
+
+    },
+
+
+]
+
+export default products
+
+
