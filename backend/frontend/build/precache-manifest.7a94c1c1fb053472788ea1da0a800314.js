4fbdb31ea3b94b0ecf0032754d92e3be19cc0b72
mehrazrumman <mehrazrumman33@icloud.com>
2024-05-14 18:37:58 +0600

    file updated

---
+++ backend/frontend/build/precache-manifest.7a94c1c1fb053472788ea1da0a800314.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9b6edfd475c23962c7a7edce391431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8963a0fa72e7269ca9dd",
+    "url": "/static/css/main.df62dd85.chunk.css"
+  },
+  {
+    "revision": "d01fb02c45429aeccc39",
+    "url": "/static/js/2.9de84dd2.chunk.js"
+  },
+  {
+    "revision": "f6c7eab08d47fd9ccbc51694170faff6",
+    "url": "/static/js/2.9de84dd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "139421cf76cf4f72653b",
+    "url": "/static/js/3.dec72598.chunk.js"
+  },
+  {
+    "revision": "8963a0fa72e7269ca9dd",
+    "url": "/static/js/main.04b65e62.chunk.js"
+  },
+  {
+    "revision": "b9db0b4ef9679c67648c",
+    "url": "/static/js/runtime-main.cb0bc770.js"
+  }
+]);
